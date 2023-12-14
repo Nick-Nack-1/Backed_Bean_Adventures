ca2a860a78c16c0f02dba9d517911e4383f50e2a
Nick-Nack-1 <nicolaasm.steenkamp@gmail.com>
2023-12-14 21:00:11 +0200

    Setup

---
+++ Maps/More.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="More" tilewidth="16" tileheight="16" tilecount="1600" columns="40">
+ <image source="mines_of_sharega.png" width="640" height="640"/>
+</tileset>
